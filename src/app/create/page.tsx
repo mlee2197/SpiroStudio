95f270beb82540f478fa79b5c18a88646479c4e1
Matt <mlee138@binghamton.edu>
2025-09-30 23:51:04 -0400

    add create page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SpiroControls from "./SprioControls";
+
+export default function CreatePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full">
+      <SpiroControls />
+    </div>
+  );
+}
